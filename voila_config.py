e7f85df71ba06df68bcdb4fad3f18f61ca232194
Shesadree <pshesadree151@gmail.com>
2026-04-19 23:55:25 -0400

    Updated config for base url for img

--- voila_config.py
+++ voila_config.py
@@ -1,3 +1,4 @@
 c.ServerApp.disable_check_xsrf = True
 c.ServerApp.allow_origin = '*'
 c.ServerApp.tornado_settings = {'xsrf_cookies': False}
+c.VoilaConfiguration.base_url = '/engine/gallery/icu-mortality-prediction-using-first-72-hours-of-mimic-iv-data/'
